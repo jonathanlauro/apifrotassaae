0dfd30c0dffffe7163e59dfb43be5d198fc2ef37
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-04-21 19:01:51 -0300

    first commit.

---
+++ frotas-SAAE/src/test/java/br/com/controlefrota/FrotasSaaeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controlefrota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrotasSaaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
