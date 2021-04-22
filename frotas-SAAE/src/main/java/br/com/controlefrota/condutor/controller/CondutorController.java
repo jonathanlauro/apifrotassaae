8c3a49ed24faf09f35a7ef895152667217755130
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-04-22 16:40:02 -0300

    busca por CNH

--- frotas-SAAE/src/main/java/br/com/controlefrota/condutor/controller/CondutorController.java
+++ frotas-SAAE/src/main/java/br/com/controlefrota/condutor/controller/CondutorController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +29,10 @@ public class CondutorController {
 	@GetMapping("/condutor/{id}")
 	public Condutor listaUnicoCondutorPorId(@PathVariable(value="id") long id ){
 		return condutorRepository.findById(id);
+	}
+	@GetMapping("/condutor/{cnh}/cnh")
+	public Condutor listaUnicoCondutorPorCNH(@PathVariable(value="cnh") String cnh ){
+		return condutorRepository.findByCNH(cnh);
 	}
 
 	@PostMapping("/condutor")
--- frotas-SAAE/src/main/java/br/com/controlefrota/condutor/repository/CondutorRepository.java
+++ frotas-SAAE/src/main/java/br/com/controlefrota/condutor/repository/CondutorRepository.java
@@ -9,4 +9,5 @@ import br.com.controlefrota.condutor.models.Condutor;
 public interface CondutorRepository extends JpaRepository<Condutor, Long> {
  
 	Condutor findById(long id);
+	Condutor findByCNH(String CNH);
 }
