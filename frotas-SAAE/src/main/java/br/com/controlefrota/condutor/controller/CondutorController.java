2e1de539b5f99d4ab63915e7cc77900a33b932ca
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-04-22 14:57:26 -0300

    controller implementado.

---
+++ frotas-SAAE/src/main/java/br/com/controlefrota/condutor/controller/CondutorController.java
@@ -0,0 +1,47 @@
+package br.com.controlefrota.condutor.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.controlefrota.condutor.models.Condutor;
+import br.com.controlefrota.condutor.repository.CondutorRepository;
+
+@RestController
+@RequestMapping(value="/")
+public class CondutorController {
+
+	@Autowired
+	CondutorRepository condutorRepository;
+	
+	@GetMapping("/condutor")
+	public List<Condutor> listaCondutor(){
+		return condutorRepository.findAll();
+	}
+	@GetMapping("/condutor/{id}")
+	public Condutor listaUnicoCondutorPorId(@PathVariable(value="id") long id ){
+		return condutorRepository.findById(id);
+	}
+
+	@PostMapping("/condutor")
+	public Condutor salvaCondutor(@RequestBody Condutor condutor) {
+		return condutorRepository.save(condutor);
+	}
+	@PatchMapping("/condutor")
+	public Condutor atualizarCondutor(@RequestBody Condutor condutor) {
+		return condutorRepository.save(condutor);
+	}
+	@DeleteMapping("/condutor/{id}")
+	public void deletarCondutor(@PathVariable(value="id") long id) {
+		condutorRepository.deleteById(id);
+	}
+}
---
+++ frotas-SAAE/src/main/java/br/com/controlefrota/condutor/repository/CondutorRepository.java
@@ -0,0 +1,12 @@
+package br.com.controlefrota.condutor.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.controlefrota.condutor.models.Condutor;
+
+public interface CondutorRepository extends JpaRepository<Condutor, Long> {
+ 
+	Condutor findById(long id);
+}
