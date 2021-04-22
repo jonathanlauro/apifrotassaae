a0e87f6f13a4e597afccc49c3b89e697288a25ab
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-04-21 22:04:57 -0300

    Criação do models e conexão com banco de dados.

---
+++ frotas-SAAE/src/main/java/br/com/controlefrota/models/Condutor.java
@@ -0,0 +1,43 @@
+package br.com.controlefrota.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_CONDUTOR")
+public class Condutor implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+	private String CNH;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCNH() {
+		return CNH;
+	}
+	public void setCNH(String cNH) {
+		CNH = cNH;
+	}
+	
+}
