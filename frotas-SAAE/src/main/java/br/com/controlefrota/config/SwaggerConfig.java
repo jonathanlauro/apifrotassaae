abc0a9835cf7d4089f220b1158be66dcf75d7ea7
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-05-29 01:09:48 -0300

    melhorias na doc

--- frotas-SAAE/src/main/java/br/com/controlefrota/config/SwaggerConfig.java
+++ frotas-SAAE/src/main/java/br/com/controlefrota/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -16,7 +17,7 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api(){
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().build();
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis( RequestHandlerSelectors.basePackage( "br.com.controlefrota" ) ).build();
 	}
 	
 	private ApiInfo apiInfo() {
