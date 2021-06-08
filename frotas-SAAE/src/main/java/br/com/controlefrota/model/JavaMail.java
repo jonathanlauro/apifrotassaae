728423d102374b388c2121ea87b59700687ae157
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-06-08 20:25:10 -0300

    Inserido filtro por veiculo.

--- frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
+++ frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
@@ -1,6 +1,7 @@
 package br.com.controlefrota.model;
 
 import javax.mail.*;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
@@ -14,7 +15,7 @@ public class JavaMail {
         Properties props = new Properties();
         props.put("email.smtp.user",remetente);
         props.put("email.smtp.protocol","smtp");
-        props.put("email.smtp.host","smtp-relay.gmail.com");
+        props.put("email.smtp.host","smtp.gmail.com");
         props.put("email.smtp.port","587");
         props.put("email.smtp.auth","true");
         props.put("email.smtp.starttls.enable","true");
@@ -28,7 +29,7 @@ public class JavaMail {
         try{
 
             MimeMessage msg = new MimeMessage(session);
-            msg.setFrom();
+            msg.setFrom(new InternetAddress(remetente));
             msg.setRecipients(Message.RecipientType.TO, destinatario);
             msg.setSubject(assunto);
             msg.setText(corpo);
