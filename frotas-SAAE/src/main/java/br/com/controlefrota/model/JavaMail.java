7db3e410ba8b881987caa9b4294032bd6aa23d76
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-06-08 20:21:05 -0300

    Inserido filtro por veiculo.

--- frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
+++ frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
@@ -14,7 +14,7 @@ public class JavaMail {
         Properties props = new Properties();
         props.put("email.smtp.user",remetente);
         props.put("email.smtp.protocol","smtp");
-        props.put("email.smtp.host","189.6.12.47");
+        props.put("email.smtp.host","smtp-relay.gmail.com");
         props.put("email.smtp.port","587");
         props.put("email.smtp.auth","true");
         props.put("email.smtp.starttls.enable","true");
