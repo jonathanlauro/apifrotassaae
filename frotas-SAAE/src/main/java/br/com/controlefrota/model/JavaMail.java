52a7d94ffc06854e4e30bd4c9d12e0c2242d3e38
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-06-08 21:50:02 -0300

    Inserido filtro por veiculo.

--- frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
+++ frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
@@ -14,12 +14,12 @@ public class JavaMail {
 
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "587");
+        props.put("mail.smtp.socketFactory.port", "465");
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smpt.starttls.enable","false");
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.port", "465");
 
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
