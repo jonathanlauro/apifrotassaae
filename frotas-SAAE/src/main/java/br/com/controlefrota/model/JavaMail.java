8bf2983ddb30d71470d7f4b0c99f41b8c086f041
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-06-09 13:36:27 -0300

    Inserido filtro por veiculo.

--- frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
+++ frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
@@ -14,14 +14,14 @@ public class JavaMail {
 
         Properties props = new Properties();
         props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.port", "587");
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smpt.starttls.enable",true);
-        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.ssl.enable", true);
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.socketFactory.fallback", "false");
+        props.put("mail.smtp.socketFactory.fallback", false);
         props.put("mail.smtp.auth", true);
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.port", "587");
 
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
@@ -38,7 +38,7 @@ public class JavaMail {
             msg.setText(corpo);
 
             Transport t = session.getTransport("smtp");
-            t.connect("smtp.gmail.com",465,"frotassaaesistem@gmail.com","!@#$1234");
+            t.connect("smtp.gmail.com",587,"frotassaaesistem@gmail.com","!@#$1234");
             t.sendMessage(msg, msg.getAllRecipients());
             t.close();
 
