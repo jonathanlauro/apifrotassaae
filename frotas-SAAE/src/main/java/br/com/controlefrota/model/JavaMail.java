dc4da39047ebb61866a1104e6226c0e9dee0db1d
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-06-09 13:23:07 -0300

    Inserido filtro por veiculo.

--- frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
+++ frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
@@ -17,6 +17,7 @@ public class JavaMail {
         props.put("mail.smtp.socketFactory.port", "465");
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smpt.starttls.enable",true);
+        props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.socketFactory.fallback", "false");
         props.put("mail.smtp.auth", true);
