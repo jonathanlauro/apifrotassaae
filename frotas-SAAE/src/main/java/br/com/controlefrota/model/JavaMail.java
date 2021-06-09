09f6b539c516d1fc104a5ab116d6c6827755bced
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-06-08 21:25:01 -0300

    Inserido filtro por veiculo.

--- frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
+++ frotas-SAAE/src/main/java/br/com/controlefrota/model/JavaMail.java
@@ -13,16 +13,11 @@ public class JavaMail {
         String senha = "!@#$1234";
 
         Properties props = new Properties();
-        props.put("email.smtp.protocol","smtp");
-        props.put("email.host","ssl://smtp.gmail.com");
-        props.put("email.smtp.auth","true");
-        props.put("email.smtp.port","25");
-        props.put("email.smtp.secure","false");
-        props.put("email.smtp.user",remetente);
-        props.put("email.smtp.starttls.enable","true");
-        props.put("mail.smtp.socketFactory.port", "25");
-        props.put("mail.smtp.socketFactory.fallback", "false");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
 
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
@@ -38,14 +33,14 @@ public class JavaMail {
             msg.setSubject(assunto);
             msg.setText(corpo);
 
-            Transport t = session.getTransport("smtp");
-            t.connect("smtp.gmail.com","frotassaaesistem@gmail.com","!@#$1234");
-            t.sendMessage(msg, msg.getAllRecipients());
-            t.close();
+//            Transport t = session.getTransport("smtp");
+//            t.connect("smtp.gmail.com","frotassaaesistem@gmail.com","!@#$1234");
+//            t.sendMessage(msg, msg.getAllRecipients());
+//            t.close();
 
 
 
-//            Transport.send(msg);
+            Transport.send(msg);
         }catch(MessagingException e){
             e.printStackTrace();
         }
